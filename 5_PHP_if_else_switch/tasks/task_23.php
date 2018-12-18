c77bd00b00e63e8bb4caa9e12c125119e637cbe8
Martin_Georgiev <45171734+martinVerGeorgiev@users.noreply.github.com>
2018-12-18 18:30:20 +0200

    Create task_23.php

---
+++ 5_PHP_if_else_switch/tasks/task_23.php
@@ -0,0 +1,21 @@
+<?php
+
+$lang='fr';
+
+switch ($lang) {
+	case 'bg':
+		echo 'Здравей, свят!';
+		break;
+	case 'en':
+		echo 'Hello, world!';
+		break;
+	case 'es':
+		echo 'Hola, mundo!';
+		break;
+	case 'de':
+		echo 'Hallo Welt!';
+		break;
+	case 'fr':
+		echo 'Bonjour tout le monde!';
+		break;
+}
